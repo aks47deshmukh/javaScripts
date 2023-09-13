474fb9cad8d3fb349c7f88937bbda5cfdf3d5d13
aks47deshmukh <138723849+aks47deshmukh@users.noreply.github.com>
2023-09-13 21:13:52 +0000

    Done

---
+++ 01_BASICS/variable.js
@@ -0,0 +1,13 @@
+ 
+//number = 2 to power 53
+//bigint
+//string = ""
+//boolean = true / false
+//null= standalone value
+//undefined =
+//symbol= unique
+//object
+
+ console.log(typeof age);  //undefined
+ console.log(typeof null)  //object
+
